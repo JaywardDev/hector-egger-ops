de05db331184f05b978dc950485186ae02e8ba79
JaywardDev <162770624+JaywardDev@users.noreply.github.com>
2026-06-08 14:48:56 +1200

    Clean up legacy stock migrations

--- supabase/migrations/20260319110000_stock_locations_code_optional.sql
+++
@@ -1,8 +0,0 @@
-alter table public.stock_locations
-  alter column code drop not null;
-
-drop index if exists public.stock_locations_code_unique_idx;
-
-create unique index stock_locations_code_unique_idx
-on public.stock_locations (code)
-where code is not null;
